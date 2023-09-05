d570f4e760915f3920e7274be793e7572f1e38b1
at0mofficial <sangwanshivam762001@gmail.com>
2023-09-05 14:10:24 -0400

    Fixed onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
     const user = await currentUser();
     if (!user) return null;
     const userInfo = await fetchUser(user.id);
-    if(userInfo.onboarded){
+    if(userInfo && userInfo.onboarded){
       redirect('/')
     }
     const userData ={
